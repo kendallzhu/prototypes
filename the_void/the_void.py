f3e82c3ebf120d891995d0cd26491954b6210cd4
kendallzhu <kzhu@college.harvard.edu>
2019-05-17 09:48:09 -0400

    fixed saving

--- the_void/the_void.py
+++ the_void/the_void.py
@@ -17,11 +17,28 @@ class Void:
         if parent in self.things:
             self.things.add_edge(parent, thing)
 
-    # return neighbors of node
+    # return string of neighbors of node
     def neighbors(self, thing):
         if thing not in self.things:
             return ''
-        return self.things[thing]
+        return '\n'.join([str(t) for t in self.things[thing]])
+
+    # replace current node and its neighbors with new node
+    def condense(self, thing, new):
+        # collect all nodes 2 away                     
+        neighbors = [n for n in self.things[thing]]
+        two_away = []
+        for n in neighbors:
+            for n2 in self.things[n]:
+                if n2 != thing and n2 not in neighbors:
+                    two_away.append(n2)
+        # remove node and all neighbors
+        self.things.remove_node(thing)
+        for n in neighbors:
+            self.things.remove_node(n)
+        # add replacement with kept edges
+        self.things.add_node(new)
+        self.things.add_edges_from([(new, n) for n in two_away])
 
     # return random neighbor (or just random node)
     def spit(self, thing = None):
@@ -45,11 +62,14 @@ class Void:
         return '\n'.join(map(lambda x: str(x), recap))
 
     def save(self, path):
-        nx.write_adjlist(self.things, path)
+        nx.write_gml(self.things, path)
 
     def load(self, path):
-        if not self.things or input('discard current session? (y or n): ') == 'y':
-            self.things = nx.read_adjlist(path)
+        if not self.things or input('discard unsaved changes? (y or n): ') == 'y':
+            try:
+                self.things = nx.read_gml(path)
+            except:
+                print('not found')
 
     def __str__(self):
         return self.recap()
@@ -66,29 +86,40 @@ while True:
         # TODO: more navigation options? delete?
         print('''COMMANDS:
         _ - create new node _ 
-        <RET> - random node
-        /n - print neighbors
-        /_ - go to node _
-        /p - print full recap
+        /c - condense node w/ neighbors
+        /n - show neighbors
+        /p - print list
+        /g - draw graph
         /s - save session
         /l - load session
+        /q - quit
         ''')
     # special commands start with /
     elif new and new[0] == '/':
         if new == '/p':
             print("\n-------------------------RECAP-------------------------")        
             print(void.recap() + '\n')
+        elif new == '/g':
             void.draw()
         elif new == '/n':
             print(void.neighbors(old))
+        elif new == '/c':
+            print(void.neighbors(old))
+            thing = input('condense ^ into (/c to cancel): \n')
+            if thing != '/c':
+                void.condense(old, thing)
+            old = thing
         elif new == '/s':
             name = input('enter name: ')
             void.save(name + '.txt')
         elif new == '/l':
             name = input('enter name: ')
             void.load(name + '.txt')
-        elif void.contains(new[1:]):
-            old = new[1:]
+        elif new == '/q':
+            # warn/suggest save?
+            break
+        else:
+            print('unrecognized command')
     # normal input
     elif new == '':
         old = void.spit(old)
